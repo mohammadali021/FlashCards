edb4ca5681a9eb613a55f1d95cfe9924b7d13f2e
MoALi <mohammadali4706@gmail.com>
2025-03-15 11:38:35 +0330

    create flashcard model

--- card/models.py
+++ card/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FlashCard(models.Model):
+    question = models.TextField(verbose_name='سوال')
+    answer = models.TextField(verbose_name='جواب')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.question[0:10]}..."
