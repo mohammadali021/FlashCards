9c9856a7601f5300d25729e6dcb7ad4e1d693710
MoALi <mohammadali4706@gmail.com>
2025-03-15 13:07:14 +0330

    add admin

--- card/admin.py
+++ card/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from card.models import FlashCard
+
+
 # Register your models here.
+@admin.register(FlashCard)
+class ModelFlashCardAdmin(admin.ModelAdmin):
+    # list_display = ('question', 'answer')
+    pass
